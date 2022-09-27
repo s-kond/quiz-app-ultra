32e28f09251c1578a7e2e1daa591e81455ffb259
Stefan Kondring <kondring.s@gmx.de>
2022-09-27 09:51:22 +0200

    corrected 'class' to 'className'

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,26 +4,26 @@ import checkedBookmark from '../assets/bookmark-clicked.svg'
 
 export default function Cards() {
     return (
-        <div class="card-container">
-            <article class="card">
+        <div className="card-container">
+            <article className="card">
                 <h2>Is this an interesting question?</h2>
-                <button type="button" class="card__answerButton">Show answer</button>
-                <div class="card__tag-container">
-                    <div class="card__tag-container_tag">#HTML</div>
-                    <div class="card__tag-container_tag">#React</div>
-                    <div class="card__tag-container_tag">#JS</div>
+                <button type="button" className="card__answerButton">Show answer</button>
+                <div className="card__tag-container">
+                    <div className="card__tag-container_tag">#HTML</div>
+                    <div className="card__tag-container_tag">#React</div>
+                    <div className="card__tag-container_tag">#JS</div>
                 </div>
-                <button type="button" class="card__bookmarkButton"><img class="bookmark" src={bookmark} alt="unchecked bookmark"/></button>
+                <button type="button" className="card__bookmarkButton"><img className="bookmark" src={bookmark} alt="unchecked bookmark"/></button>
             </article>
-            <article class="card">
+            <article className="card">
                 <h2>Maybe this one's better?</h2>
-                <button type="button" class="card__answerButton">Show answer</button>
-                <div class="card__tag-container">
-                    <div class="card__tag-container_tag">#HTML</div>
-                    <div class="card__tag-container_tag">#React</div>
-                    <div class="card__tag-container_tag">#JS</div>
+                <button type="button" className="card__answerButton">Show answer</button>
+                <div className="card__tag-container">
+                    <div className="card__tag-container_tag">#HTML</div>
+                    <div className="card__tag-container_tag">#React</div>
+                    <div className="card__tag-container_tag">#JS</div>
                 </div>
-                <button type="button" class="card__bookmarkButton"><img class="bookmark" src={checkedBookmark} alt="checked bookmark"/></button>
+                <button type="button" className="card__bookmarkButton"><img className="bookmark" src={checkedBookmark} alt="checked bookmark"/></button>
             </article>
         </div>
     )
