614767be4fe0f4ee0e4b9490ed12a1c8bb9f6154
Stefan Kondring <kondring.s@gmx.de>
2022-09-27 12:40:19 +0200

    Add props for cards

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Header from './components/header/Header';
 import Nav from './components/navigation/Navigation';
-import Cards from './components/card/Card';
+import Card from './components/card/Card';
 
 function App() {
   return (
     <div className="App">
      <Header/>
-     <Cards/>
+     <div className="card-container">
+        <Card question="Are penguins able to fly?" answer="Unfortunately not" tag="penguins" bookmarked={false}/>
+        <Card question="What do penguins like the most?" answer="Fish" tag="penguins" bookmarked={true}/>
+        <Card question="Do penguins live north or south of the equator?" answer="South" tag="penguins" bookmarked={false}/>
+     
+     </div>
      <Nav/>
     </div>
   );
