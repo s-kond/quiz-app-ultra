cf3f93020177d36172bea5c7e1f02c86fb5495ea
Stefan Kondring <kondring.s@gmx.de>
2022-09-27 16:40:29 +0200

    add tags-/cards-array

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import Header from './components/header/Header';
 import Nav from './components/navigation/Navigation';
 import Card from './components/card/Card';
 
+const cards = [
+  {id: 101, question: "Are penguins able to fly?", answer: "Unfortunately not", tags: ["penguins", "sea"], bookmarked: false},
+  {id: 102, question: "What do penguins like the most?", answer:"Fish", tags: ["penguins", "yolo", "fish"], bookmarked: true},
+  {id: 102, question: "Do penguins live north or south of the equator?", answer:"South", tags: ["penguins", "equator"], bookmarked: false}
+]
+
 function App() {
   return (
     <div className="App">
      <Header/>
      <div className="card-container">
-        <Card question="Are penguins able to fly?" answer="Unfortunately not" tag="penguins" bookmarked={false}/>
-        <Card question="What do penguins like the most?" answer="Fish" tag="penguins" bookmarked={true}/>
-        <Card question="Do penguins live north or south of the equator?" answer="South" tag="penguins" bookmarked={false}/>
+        {cards.map(card => {
+          return (
+            <Card id={card.id} question={card.question} answer={card.answer} tags={card.tags} bookmarked={card.bookmarked} />
+          )
+        })}
      </div>
      <Nav/>
     </div>
