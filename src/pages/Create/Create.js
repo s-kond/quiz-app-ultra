c93020383d4676c0cf0919aec8368682a2289b3c
Stefan Kondring <kondring.s@gmx.de>
2022-09-28 17:15:27 +0200

    add empty create-page

---
+++ src/pages/Create/Create.js
@@ -0,0 +1,9 @@
+import "./Create.css"
+
+export default function Create(){
+    return (
+    <>
+    <h2>The Create-Page is currently under construction</h2>
+    <div className="container"></div>
+    </>)
+}
