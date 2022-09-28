67edef96c954d1b5f54aa4153f83bb456574abce
Stefan Kondring <kondring.s@gmx.de>
2022-09-28 17:15:40 +0200

    add empty profile-page

---
+++ src/pages/Profile/Profile.js
@@ -0,0 +1,9 @@
+import "./Profile.css"
+
+export default function Profile(){
+    return (
+    <>
+    <h2>The Profile-Page is currently under construction</h2>
+    <div className="container"></div>
+    </>)
+}
