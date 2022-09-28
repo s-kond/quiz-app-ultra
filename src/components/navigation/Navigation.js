8455ad7b130c15b42a458e4dec02ef10e41d0c2d
Stefan Kondring <kondring.s@gmx.de>
2022-09-28 17:17:30 +0200

    lift state-handling to app.js

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,25 +1,22 @@
 import "./Navigation.css"
-import { useState } from "react";
 import homeIcon from "../assets/home-svgrepo-com.svg";
 import bookmarkIcon from "../assets/bookmark-clicked.svg";
 import profileIcon from "../assets/profile-user-svgrepo-com.svg";
 import addIcon from "../assets/add-svgrepo-com.svg"
 
-export default function Nav(){
-    const [activePage, setActivePage] = useState("home")
-    
+export default function Nav({setPage, page}){
     return (
         <nav>
-            <button type="button" className={activePage === "home" ? "nav__button nav__button-active" : "nav__button"} onClick = {() => setActivePage("home")}>
+            <button type="button" className={page === "home" ? "nav__button nav__button-active" : "nav__button"} onClick = {() => setPage("home")}>
                 <img className="nav__button__img" src={homeIcon} alt="home icon"/>
             </button>
-            <button type="button" className={activePage === "bookmark" ? "nav__button nav__button-active" : "nav__button"} onClick = {() => setActivePage("bookmark")}>
+            <button type="button" className={page === "bookmark" ? "nav__button nav__button-active" : "nav__button"} onClick = {() => setPage("bookmark")}>
                 <img className="nav__button__img" src={bookmarkIcon} alt="bookmark icon"/>
             </button>
-            <button type="button" className={activePage === "add" ? "nav__button nav__button-active" : "nav__button"} onClick = {() => setActivePage("add")}>
+            <button type="button" className={page === "add" ? "nav__button nav__button-active" : "nav__button"} onClick = {() => setPage("add")}>
                 <img className="nav__button__img" src={addIcon} alt="add card icon"/>
             </button>
-            <button type="button" className={activePage === "profile" ? "nav__button nav__button-active" : "nav__button"} onClick = {() => setActivePage("profile")}>
+            <button type="button" className={page === "profile" ? "nav__button nav__button-active" : "nav__button"} onClick = {() => setPage("profile")}>
                 <img className="nav__button__img" src={profileIcon} alt="profile icon"/>
             </button>
         </nav>
