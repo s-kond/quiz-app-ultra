d19acceae7afcece2cf19c6e4d418c079d005d39
Stefan Kondring <kondring.s@gmx.de>
2022-09-28 12:38:48 +0200

    small formatting

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -10,10 +10,18 @@ export default function Nav(){
     
     return (
         <nav>
-            <button type="button" className={activePage === "home" ? "nav__button nav__button-active" : "nav__button"} onClick = {() => setActivePage("home")}><img className="nav__button__img" src={homeIcon} alt="home icon"/></button>
-            <button type="button" className={activePage === "bookmark" ? "nav__button nav__button-active" : "nav__button"} onClick = {() => setActivePage("bookmark")}><img className="nav__button__img" src={bookmarkIcon} alt="bookmark icon"/></button>
-            <button type="button" className={activePage === "add" ? "nav__button nav__button-active" : "nav__button"} onClick = {() => setActivePage("add")}><img className="nav__button__img" src={addIcon} alt="add card icon"/></button>
-            <button type="button" className={activePage === "profile" ? "nav__button nav__button-active" : "nav__button"} onClick = {() => setActivePage("profile")}><img className="nav__button__img" src={profileIcon} alt="profile icon"/></button>
+            <button type="button" className={activePage === "home" ? "nav__button nav__button-active" : "nav__button"} onClick = {() => setActivePage("home")}>
+                <img className="nav__button__img" src={homeIcon} alt="home icon"/>
+            </button>
+            <button type="button" className={activePage === "bookmark" ? "nav__button nav__button-active" : "nav__button"} onClick = {() => setActivePage("bookmark")}>
+                <img className="nav__button__img" src={bookmarkIcon} alt="bookmark icon"/>
+            </button>
+            <button type="button" className={activePage === "add" ? "nav__button nav__button-active" : "nav__button"} onClick = {() => setActivePage("add")}>
+                <img className="nav__button__img" src={addIcon} alt="add card icon"/>
+            </button>
+            <button type="button" className={activePage === "profile" ? "nav__button nav__button-active" : "nav__button"} onClick = {() => setActivePage("profile")}>
+                <img className="nav__button__img" src={profileIcon} alt="profile icon"/>
+            </button>
         </nav>
     )
 }
