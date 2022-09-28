123a34681619ce3f76226718bd1ab59b681a7d6d
Stefan Kondring <kondring.s@gmx.de>
2022-09-28 12:16:17 +0200

    add toggle to answer button

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,13 +1,16 @@
 import "./Card.css"
+import { useState } from "react";
 import bookmark from '../assets/bookmark.svg'
 import checkedBookmark from '../assets/bookmark-clicked.svg'
 
 export default function Card({question, answer, tags, bookmarked = false}) {
+    const [showAnswer, setShowAnswer] = useState(false);
+    
     return (
         <article className="card">
             <h2>{question}</h2>
-            <button type="button" className="card__answerButton">Hide answer</button>
-            <p className="card__answer">{answer}</p>
+            <button type="button" className="card__answerButton" onClick={()=> setShowAnswer(!showAnswer)}>{showAnswer ? "Hide answer" : "Show answer"}</button>
+            <p className={showAnswer ? "card__answer" : "card__answer hidden"}>{answer}</p>
             <div className="card__tag-container">
                 {tags.map(tag => {
                     return <div key={Math.random()} className="card__tag-container_tag">#{tag}</div>
